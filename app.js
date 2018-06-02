808b57330140466fe8fef77f80a58f2be9785e4e
grindos <grisha_ya@inbox.ru>
2018-06-02 19:10:46 +0300

    Serve pages as handlebars.js templates

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ const path = require('path')
 
 let app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>')
-  res.send({
-    name: 'Greg',
-    likes: [
-      'books',
-      'comics'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About Page')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.get('/bad', (req, res) => {
